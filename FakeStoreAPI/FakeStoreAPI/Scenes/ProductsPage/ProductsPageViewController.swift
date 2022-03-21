4616495e29f73b1f3c80fc87a87f6fb3a8dea633
ealpago <alpagoemre@icloud.com>
2022-03-21 15:50:43 +0300

    ADDED: Demo storyboard

---
+++ FakeStoreAPI/FakeStoreAPI/Scenes/ProductsPage/ProductsPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProductsPageViewController.swift
+//  FakeStoreAPI
+//
+//  Created by Emre Alpago on 21.03.2022.
+//
+
+import UIKit
+
+class ProductsPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+}
