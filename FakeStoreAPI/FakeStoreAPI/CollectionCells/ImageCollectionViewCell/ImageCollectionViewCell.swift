6d3d3fa7a55c3e0c5031fcbf66d25db784167b43
ealpago <alpagoemre@icloud.com>
2022-03-22 16:38:14 +0300

    ImageCollectionView worked (not dynamic)

--- FakeStoreAPI/FakeStoreAPI/CollectionCells/ImageCollectionViewCell/ImageCollectionViewCell.swift
+++ FakeStoreAPI/FakeStoreAPI/CollectionCells/ImageCollectionViewCell/ImageCollectionViewCell.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ImageCollectionViewCell: UICollectionViewCell {
     
+    static let ImageCollectionViewCellIdentifier = "ImageCollectionViewCell"
+    
     @IBOutlet var imageView:UIImageView?
+    
+    private var itemModel: CollectionViewItemModel?
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func setupCell(cellModel: CollectionViewItemModel) {
+        itemModel = cellModel
+        if let itemModel = itemModel {
+            imageView?.image = UIImage(named: itemModel.imageView ?? "getir")
+        }
     }
 
 }
--- FakeStoreAPI/FakeStoreAPI/Scenes/ProductsPage/ProductsPageViewController.swift
+++ FakeStoreAPI/FakeStoreAPI/Scenes/ProductsPage/ProductsPageViewController.swift
@@ -12,16 +12,17 @@ class ProductsPageViewController: UIViewController {
     @IBOutlet var imageCollectionView: UICollectionView?
     @IBOutlet var categoriesCollectionView: UICollectionView?
     
-//    var imageCollectionViewCells:[CollectionViewModel] = []
+    var imageCollectionViewCells:[CollectionViewModel] = []
     var categoriesCollectionViewCells:[CollectionViewModel] = []
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//
-//        imageCollectionView?.register(UINib(nibName: "ImageCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "ImageCollectionViewCell")
-//        imageCollectionView?.delegate = self
-//        imageCollectionView?.dataSource = self
+        
+        imageCollectionView?.register(UINib(nibName: "ImageCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "ImageCollectionViewCell")
+        imageCollectionView?.delegate = self
+        imageCollectionView?.dataSource = self
+        
         categoriesCollectionView?.register(UINib(nibName: "CategoriesCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CategoriesCollectionViewCell")
         categoriesCollectionView?.delegate = self
         categoriesCollectionView?.dataSource = self
@@ -34,44 +35,60 @@ class ProductsPageViewController: UIViewController {
         collectionViewItemModelArray.append(CollectionViewItemModel(cellType: .categories, imageView: "damacana2", category: "kategori", label: "Damacana", price: nil))
         collectionViewItemModelArray.append(CollectionViewItemModel(cellType: .categories, imageView: "damacana2", category: "kategori", label: "Damacana", price: nil))
         collectionViewItemModelArray.append(CollectionViewItemModel(cellType: .categories, imageView: "damacana2", category: "kategori", label: "Damacana", price: nil))
-
+        collectionViewItemModelArray.append(CollectionViewItemModel(cellType: .categories, imageView: "damacana2", category: "kategori", label: "Damacana", price: nil))
+        collectionViewItemModelArray.append(CollectionViewItemModel(cellType: .categories, imageView: "damacana2", category: "kategori", label: "Damacana", price: nil))
+        collectionViewItemModelArray.append(CollectionViewItemModel(cellType: .categories, imageView: "damacana2", category: "kategori", label: "Damacana", price: nil))
+        collectionViewItemModelArray.append(CollectionViewItemModel(cellType: .categories, imageView: "damacana2", category: "kategori", label: "Damacana", price: nil))
+        collectionViewItemModelArray.append(CollectionViewItemModel(cellType: .categories, imageView: "damacana2", category: "kategori", label: "Damacana", price: nil))
         categoriesCollectionViewCells.append(CollectionViewModel(items: collectionViewItemModelArray))
         
-        // Do any additional setup after loading the view.
+        var imageViewItemModelArray:[CollectionViewItemModel] = []
+        imageViewItemModelArray.append(CollectionViewItemModel(cellType: .image, imageView: "getir", category: nil, label: nil, price: nil))
+        imageViewItemModelArray.append(CollectionViewItemModel(cellType: .image, imageView: "getir", category: nil, label: nil, price: nil))
+        imageViewItemModelArray.append(CollectionViewItemModel(cellType: .image, imageView: "getir", category: nil, label: nil, price: nil))
+        imageViewItemModelArray.append(CollectionViewItemModel(cellType: .image, imageView: "getir", category: nil, label: nil, price: nil))
+        imageCollectionViewCells.append(CollectionViewModel(items: imageViewItemModelArray))
     }
-    
-//    func managingData(){
-//        NetworkManager.service.request(requestRoute: .getCategories, responseModel: FakeAPIResponse.self) { [weak self] details in
-//            guard let categories = details.category else {return}
-//            guard let self = self else {return}
-//
-//            var categoryArray:[CollectionViewItemModel] = []
-//
-//            for item in categories {
-//                let categoryModel = CollectionViewItemModel(cellType: .categories, imageView: "damacana2", label:"Deneme")
-//                categoryArray.append(categoryModel)
-//            }
-//            self.categoriesCollectionViewCells.append(CollectionViewModel(items: categoryArray))
-//            DispatchQueue.main.async {
-//                self.categoriesCollectionView?.reloadData()
-//            }
-//        }
-//    }
 }
 
-extension ProductsPageViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+extension ProductsPageViewController: UICollectionViewDelegate, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return categoriesCollectionViewCells[section].items.count
+        if (collectionView == imageCollectionView) {
+            return imageCollectionViewCells[section].items.count
+        }else{
+            return categoriesCollectionViewCells[section].items.count
+        }
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        categoriesCollectionViewCells.count
+        if (collectionView == imageCollectionView) {
+            return imageCollectionViewCells.count
+        }else {
+            return categoriesCollectionViewCells.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoriesCollectionViewCell.CategoriesCollectionViewCellIdentifier, for: indexPath) as! CategoriesCollectionViewCell
-        let cellModel = categoriesCollectionViewCells[indexPath.section].items[indexPath.row]!
-        cell.setupCell(cellModel: cellModel)
-        return cell
+        
+        if (collectionView == imageCollectionView) {
+            let cell2 = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.ImageCollectionViewCellIdentifier, for: indexPath) as! ImageCollectionViewCell
+            let cellModel = imageCollectionViewCells[indexPath.section].items[indexPath.row]!
+            cell2.setupCell(cellModel: cellModel)
+            
+            return cell2
+            
+        
+        }else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoriesCollectionViewCell.CategoriesCollectionViewCellIdentifier, for: indexPath) as! CategoriesCollectionViewCell
+            let cellModel = categoriesCollectionViewCells[indexPath.section].items[indexPath.row]!
+            cell.setupCell(cellModel: cellModel)
+            return cell
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if (collectionView == imageCollectionView) {
+            return CGSize(width: 390, height: 120)
+        }
+        return CGSize(width: 80, height: 100)
     }
 }
