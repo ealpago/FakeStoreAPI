289e202352c690644f6e07dffdc7e9cc027a494c
ealpago <alpagoemre@icloud.com>
2022-03-22 12:19:21 +0300

    ADDED: CategoriesCollectionView with scroll

---
+++ FakeStoreAPI/FakeStoreAPI/Scenes/ProductsPage/CollectionCells/CategoriesCollectionViewCell/CategoriesCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CategoriesCollectionViewCell.swift
+//  FakeStoreAPI
+//
+//  Created by Emre Alpago on 22.03.2022.
+//
+
+import UIKit
+
+class CategoriesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView?
+    @IBOutlet var categoryName: UILabel?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
