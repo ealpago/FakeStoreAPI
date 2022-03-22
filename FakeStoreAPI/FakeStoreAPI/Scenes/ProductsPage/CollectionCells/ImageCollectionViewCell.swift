17a341485a060bac5f74eac7c35b49dbfc4a721c
ealpago <alpagoemre@icloud.com>
2022-03-22 10:48:22 +0300

    ADDED: ImageCollectionViewCell

---
+++ FakeStoreAPI/FakeStoreAPI/Scenes/ProductsPage/CollectionCells/ImageCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ImageCollectionViewCell.swift
+//  FakeStoreAPI
+//
+//  Created by Emre Alpago on 22.03.2022.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView:UIImageView?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
